13be0b97da2f6fb953ea5e7c909a4e5d2105bb53
Efe <knowledgeagare157@gmail.com>
2020-02-08 20:05:13 +0100

    apply apploading to load font before the app start

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View } from "react-native";
 
 export class CategoriesScreen extends Component {
-    render() {
-        return (
-            <View style={styles.screen}>
-                <Text> CategoriesScreen  </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.screen}>
+        <Text> CategoriesScreen </Text>
+      </View>
+    );
+  }
 }
 
-export default CategoriesScreen
+export default CategoriesScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignContent: 'center',
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center"
+  }
 });
