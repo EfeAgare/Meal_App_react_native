a14f81cdfba7de41c6165341c5e2c905bfa2157e
Efe <knowledgeagare157@gmail.com>
2020-02-09 11:44:17 +0100

    outputting a grid of item using FlatList

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
 
+const renderGridItem = ({ item }) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{item.title}</Text>
+    </View>
+  );
+};
 export class CategoriesScreen extends Component {
   render() {
     return (
-      <View style={styles.screen}>
-        <Text> CategoriesScreen! </Text>
-        <Button
-          title='Go to Meals!'
-          onPress={() => {
-            this.props.navigation.navigate({ routeName: 'CategoryMeals' });
-          }}
-        />
-      </View>
+      <FlatList
+        data={CATEGORIES}
+        numColumns={2}
+        renderItem={renderGridItem}
+        keyExtractor={item => item.id}
+      />
+      // <View style={styles.screen}>
+      //   <Text> CategoriesScreen! </Text>
+      //   <Button
+      //     title='Go to Meals!'
+      //     onPress={() => {
+      //       this.props.navigation.navigate({ routeName: 'CategoryMeals' });
+      //     }}
+      //   />
+      // </View>
     );
   }
 }
@@ -23,6 +37,10 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
   }
 });
