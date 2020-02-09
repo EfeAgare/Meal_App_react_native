19c35d4d9c8f27ef1cb9c3feeb0b8d72658bd374
Efe <knowledgeagare157@gmail.com>
2020-02-09 14:36:23 +0100

    loading meal base on  category selected

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Platform } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 export class CategoryMealScreen extends Component {
-  render() {
-
+  renderMealItem = ({ item }) => {
     return (
-      <View style={styles.screen}>
-        <Text> CategoryMealScreen </Text>
-        <Button
-          title='Go to Meal Details!'
-          onPress={() => {
-            this.props.navigation.navigate('MealDetails');
-          }}
-        />
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          this.props.navigation.navigate({
+            routeName: 'MealDetails',
+            params: { mealId: item.id }
+          });
+        }}
+      >
+        <View style={styles.screen}>
+          <Text> {item.title} </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  render() {
+    const categoryId = this.props.navigation.getParam('categoryId');
+
+    const displayMeals = MEALS.filter(
+      meal => meal.categoryIds.indexOf(categoryId) >= 0
+    );
+    return (
+      <FlatList
+        data={displayMeals}
+        keyExtractor={item => item.id}
+        renderItem={this.renderMealItem}
+      />
     );
   }
 }
