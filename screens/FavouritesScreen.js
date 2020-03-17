858b5760a846dfb35aff634f0309c4fc76fdab95
Efe <knowledgeagare157@gmail.com>
2020-03-17 19:58:23 +0100

    rendering a fallback text

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,13 +1,24 @@
 import React from "react";
 import MealList from "../components/shared/MealList";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { StyleSheet, View } from "react-native";
 import { useSelector } from "react-redux";
 
 import CustomHeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/shared/DefaultText";
 
 const FavouritesScreen = props => {
   const favMeal = useSelector(state => state.meals.favouriteMeals);
 
+  if (favMeal.length === 0 || !favMeal) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>
+          No Favourite meals added yet. Start adding some.
+        </DefaultText>
+      </View>
+    );
+  }
   return <MealList displayMeals={favMeal} navigation={props.navigation} />;
 };
 
@@ -28,4 +39,11 @@ FavouritesScreen.navigationOptions = navData => {
   };
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 export default FavouritesScreen;
